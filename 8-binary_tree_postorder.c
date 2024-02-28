82bb355a98e5b7b328c64dd333d348a6274993fb
Benjamin <benjminrichie@gmail.com>
2024-02-28 00:05:38 -0800

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,9 +5,9 @@
  * binary tree using post-order traversal
  *
  * @tree: Just a pointer
- * 
+ *
  * @func: Just a pointer to a function to call for each node.
- * 
+ *
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
